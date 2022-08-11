7a6dec2d9792434d9f4d9c98a94afbf9cee0a9bd
Afonasev Evgeniy <ea.afonasev@gmail.com>
2022-08-12 02:24:19 +0500

    remove tests/tmp from vcs

--- tests/tmp/simple/simple/__init__.py
+++
@@ -1,3 +0,0 @@
-
-def get_numbers_sum(a: int, b: int) -> int:
-    return a + b
--- tests/tmp/simple/tests/test_simple.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from simple import get_numbers_sum
-
-
-def test_get_numbers_sum():
-    assert get_numbers_sum(1, 2) == 3
